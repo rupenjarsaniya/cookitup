57428507a0abfffbaa0e200dea3291ff5d61e66a
rupen11 <jarsaniyarupen7984@gmail.com>
2022-07-24 16:54:43 +0530

    cookitup

--- middlewares/profileimage.js
+++ middlewares/profileimage.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import httpStatusCodes from "../helpers/httpStatusCodes";
 
-const DIR = '/public/userprofileimg/';
+const DIR = '/userprofileimg/';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
